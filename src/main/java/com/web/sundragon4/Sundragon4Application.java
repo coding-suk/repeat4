54059289fdfc388148cee8ea4b4a0598ac4f974c
developer1 <dev1@email.com>
2024-09-10 13:35:48 +0900

    first commit

---
+++ src/main/java/com/web/sundragon4/Sundragon4Application.java
@@ -0,0 +1,13 @@
+package com.web.sundragon4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sundragon4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sundragon4Application.class, args);
+    }
+
+}
